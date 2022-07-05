a65cbd5f3cc0b97d46c48558c8db8faa80728d1f
aslanchek <taibov.ag@phystech.edu>
2022-07-06 02:45:29 +0300

    working version, without gui, ant append by clicking

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Langton-s-ant
+# Langton's ant model
+App written with Pygame which provides the abilities to create langton's ants, to collide them, to control simulation speed and etc. (dev)
+## Description
+### Game control 
+* use mouse to create langton's ant exact in the click pos
+* ```Q``` - quit
+* ```W``` - increase fps (to speed up simulation)
+* ```S``` - decrease fps (to slow down simulation)
+
+## Requirements
+* Python
+* Pygame
+## Usage
+Starting via ```main.py```:
+```bash
+python3 Main.py
+````
---
+++ main.py
@@ -0,0 +1,86 @@
+import pygame, pygame.locals, pygame.draw
+import constants
+
+class Button():
+    pass
+
+class Ant:
+    def __init__(self, color, start_x, start_y, start_direction, game):
+        self.color = color
+        self.x = start_x
+        self.y = start_y
+        self.game = game
+        self.direction = start_direction
+    
+    def move(self):
+        position = (self.x, self.y)
+        if position in self.game.filled_dots:
+            self.direction = (self.direction + 4 - 1) % 4 # angle: 1 for right, -1 for left
+            del self.game.filled_dots[position] # turn <color> in white
+        else:
+            self.direction = (self.direction + 4 + 1) % 4 
+            self.game.filled_dots[position] = self.color # turns white in <color>
+
+        self.x += constants.DIRECTIONS[self.direction][0]
+        self.y += constants.DIRECTIONS[self.direction][1]
+
+
+class Game:
+    def __init__(self, screen):
+        self.running = True
+        self.screen = screen
+        self.fps = constants.FPS_DEFAULT
+        self.selected_color = constants.BLACK
+        self.selected_direction = 0
+        self.filled_dots = {}
+        self.epoch = 1
+        self.ants = []
+
+    def next_epoch(self):
+        for ant in self.ants:
+            ant.move()
+        self.epoch += 1
+        
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                    self.stop() 
+            elif event.type == pygame.locals.KEYDOWN:
+                if event.key == pygame.locals.K_q:
+                    self.stop()
+                if event.key == pygame.locals.K_w:
+                    self.fps += 10
+                if event.key == pygame.locals.K_s:
+                    self.fps -= 10
+                if event.key == pygame.locals.K_d:
+                    print("=========DEBUG INFO==========")
+                    print() 
+                    print("=============================")
+            elif event.type == pygame.locals.MOUSEBUTTONDOWN:
+                self.ants.append(Ant(self.selected_color, event.pos[0], event.pos[1], self.selected_direction, self))
+    
+    def render(self):
+        self.screen.fill(constants.WHITE)
+        for dot in self.filled_dots:
+            pygame.draw.rect(self.screen, self.filled_dots[dot], (dot[0], dot[1], constants.SIZE, constants.SIZE))
+        pygame.display.update()
+
+    def stop(self):
+        self.running = False
+
+    def start(self):
+        clock = pygame.time.Clock()
+
+        while self.running:
+            clock.tick(self.fps)
+            self.render()
+            self.handle_events()
+            self.next_epoch()
+
+            pygame.display.set_caption(f'FPS: {int(clock.get_fps())}, EPOCH: {self.epoch}')
+
+pygame.init()
+screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT), pygame.SCALED) 
+
+main = Game(screen)
+main.start()
