6ab47de1b403fd334ce43454231d2c21e5f248a7
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2022-07-09 14:56:20 +0300

    Created faster field structure

---
+++ chunks.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+SIZE = 32
+
+class Chunk:
+    def __init__(self, arr=None):
+        self.arr = np.zeros(SIZE**2, dtype=int)
+
+
+    def __getitem__(self, cors):
+        x, y = cors
+        return self.arr[y * SIZE + x]
+
+    def __setitem__(self, cors, value):
+        x, y = cors
+        self.arr[y * SIZE + x] = value
+
+
+class Field:
+    def __init__(self):
+        self.field = {}
+
+    def __getitem__(self, cors):
+        chunk_cors, in_chunk_cors = Field.translate_cors(cors)
+
+        if chunk_cors in self.field:
+            return self.field[chunk_cors][in_chunk_cors]
+
+        return self.field[chunk_cors][in_chunk_cors]
+
+    def __setitem__(self, cors, value):
+        chunk_cors, in_chunk_cors = Field.translate_cors(cors)
+
+        if chunk_cors not in self.field:
+            self.field[chunk_cors] = Chunk()
+
+        self.field[chunk_cors][in_chunk_cors] = value
+
+
+    @classmethod
+    def translate_cors(cors):
+        in_chunk_x, in_chunk_y = x % SIZE, y % SIZE
+        in_chunk_cors = (in_chunk_x, in_chunk_y)
+
+        chunk_x, chunk_y = x // SIZE, y // SIZE
+        chunk_cors = (chunk_x, chunk_y)
+
+        return chunk_cors, in_chunk_cors
